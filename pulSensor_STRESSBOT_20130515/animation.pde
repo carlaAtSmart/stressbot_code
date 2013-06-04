2f449e653a3fac1e3282c6151e6479958ac8addb
Steve Faletti <steve.faletti@smartdesignworldwide.com>
2013-06-04 16:11:51 -0400

    Wave Drawn as Curve Added

---
+++ pulSensor_STRESSBOT_20130515/animation.pde
@@ -0,0 +1,48 @@
+int frame = 0; // The frame to display
+int textX = 512;
+int textY = 530;  
+PImage[] images = new PImage[7]; // Image array
+
+void defineFaceAnim() {
+  images[0] = loadImage("botFace1-01.png");
+  images[1] = loadImage("botFace2-01.png");
+  images[2] = loadImage("botFace3-01.png");
+  images[3] = loadImage("botFace0-01.png");
+  images[4] = loadImage("botFace3-01.png");
+  images[5] = loadImage("botFace2-01.png");
+  images[6] = loadImage("botFace1-01.png");
+}
+
+void drawFaceAnim() {
+  frameRate(15);
+  frame++;
+  if (frame == images.length) {
+    frame = 0;
+    delay(int(random(1000, 3000)));
+  }
+  image(images[frame], 0, 0);
+
+  textFont(botFont); 
+  fill(255, 253, 248);            // eggshell white
+  textAlign(CENTER);
+  text("Hello! Place your finger on the sensor to measure your stress.", textX, textY);
+}
+
+void confirmation() {
+  background(0);//clear
+  image(images[3], 0, 0);
+  textFont(botFont); 
+  fill(255, 253, 248);            // eggshell white
+  text("Great! Now stay still while I read your heart rate.", textX, textY);
+}
+
+void putFingerBack() {
+  frameRate(15);
+  frame++;
+  if (frame == images.length) {
+    frame = 0;
+    delay(int(random(1000, 3000)));
+  }
+  image(images[frame], 0, 0);
+}
+
--- pulSensor_STRESSBOT_20130515/build-tmp/source/pulSensor_STRESSBOT_20130515.java
+++ pulSensor_STRESSBOT_20130515/build-tmp/source/pulSensor_STRESSBOT_20130515.java
@@ -17,20 +17,20 @@ import java.io.IOException;
 public class pulSensor_STRESSBOT_20130515 extends PApplet {
 
 /*     PulseSensor Amped HRV Poncaire Plot
- 
- This is an HRV visualizer code for Pulse Sensor.
- Use this with PulseSensorAmped_Arduino_1.1 Arduino code and the Pulse Sensor Amped hardware.
- This code will draw a Poncaire Plot of the IBI (InterBeat Interval) passed from Arduino.
- The Poncaire method of visualizing HRV trends is to plot the current IBI against the last IBI. 
- key press commands included in this version:
- press 'S' or 's' to take a picture of the data window. (.jpg image)
- press 'c' to clear the graph 
- Created by Joel Murphy, early 2013
- This code released into the public domain without promises or caveats.
- */
 
- 
- Serial port;
+This is an HRV visualizer code for Pulse Sensor.
+Use this with PulseSensorAmped_Arduino_1.1 Arduino code and the Pulse Sensor Amped hardware.
+This code will draw a Poncaire Plot of the IBI (InterBeat Interval) passed from Arduino.
+The Poncaire method of visualizing HRV trends is to plot the current IBI against the last IBI. 
+key press commands included in this version:
+press 'S' or 's' to take a picture of the data window. (.jpg image)
+press 'c' to clear the graph 
+Created by Joel Murphy, early 2013
+This code released into the public domain without promises or caveats.
+*/
+
+
+Serial port;
 
 int test;                 // general debugger
 int pulseRate = 0;        // used to hold pulse rate value from arduino (updated in serialEvent)
@@ -38,7 +38,7 @@ int Sensor = 0;           // used to hold raw sensor data from arduino (updated 
 int IBI;                  // length of time between heartbeats in milliseconds (updated in serialEvent)
 int ppgY;                 // used to print the pulse waveform
 
-ArrayList<Integer> beatIntervals;
+ArrayList<Integer> beatIntervals; //store each beat interval in an Array List so we can compare multiple values over time
 
 // initializing flags here
 boolean pulse = false;    // made true in serialEvent when processing gets new IBI value from arduino
@@ -46,57 +46,69 @@ boolean pulse = false;    // made true in serialEvent when processing gets new I
 
 public void setup() {
   background(255);
-  size(1024, 600, P3D); // Stage size
-  // screenMask = loadImage("stressbot-screen-mask.png");
+size(1024, 600, P3D); // Stage size
+// screenMask = loadImage("stressbot-screen-mask.png");
 
-  beatIntervals = new ArrayList(); //create empty array list
+beatIntervals = new ArrayList(); //create empty array list
 
-  // FIND AND ESTABLISH CONTACT WITH THE SERIAL PORT
-  println(Serial.list());       // print a list of available serial ports
-  port = new Serial(this, Serial.list()[0], 115200); // choose the right baud rate
-  port.bufferUntil('\n');          // arduino will end each ascii number string with a carriage return 
-  port.clear();                    // flush the Serial buffer
+// FIND AND ESTABLISH CONTACT WITH THE SERIAL PORT
+println(Serial.list());       // print a list of available serial ports
+port = new Serial(this, Serial.list()[0], 115200); // choose the right baud rate
+port.bufferUntil('\n');          // arduino will end each ascii number string with a carriage return 
+port.clear();                    // flush the Serial buffer
 }  // END OF SETUP
 
 //----------------------------------------------------------------
 public void draw() {
-  background(255, 10);
-  if (beatIntervals.size() > 20) { //take 20 beats to calibrate
-    drawPoincare();
+background(255);
+int beatsToMeasure = 20;
+if (beatIntervals.size() > beatsToMeasure) { //take beatsToMeasure beats to calibrate
+  //drawPoincare();
+  calculateWaveVariance(beatsToMeasure); //
   }  
 }
 
-public void drawPoincare() {
-  curveTightness(map(mouseX, 0, width, -5, 5));
-  pushStyle();
-  noFill();
-  beginShape();
+public void calculateWaveVariance(int _sampleSize) {
+  int[] sampleArray = new int[_sampleSize];
   int counter = 0;
-      for (int i=beatIntervals.size()-16; i<beatIntervals.size(); i++) { //get the last 30 values
-        float xPos = beatIntervals.get(i-1);
-        xPos = normalizeIntervalVal(xPos, width); //normalize x-value
-        float yPos = beatIntervals.get(i);
-        yPos = normalizeIntervalVal(yPos, height); //get and normalize y-value
-        curveVertex(xPos, yPos, 0);
-      }
-      endShape();
-      popStyle();
-    }
+  for (int i=beatIntervals.size()-_sampleSize; i<beatIntervals.size(); i++) { //load native array with sample values
+    sampleArray[counter] = beatIntervals.get(i);
+    counter++;
+  }
+  drawIntervalWave(sampleArray);
+}
 
-    public float normalizeIntervalVal(float _arrayVal, int _screenSize) {
-      float _val = map(_arrayVal, 0, maxInterval(), 30, _screenSize-30);
-      return _val;
-    }
 
-    public int maxInterval() {
-      int maxVal = 0;
-      for (int i=0; i<beatIntervals.size(); i++) {
-        if (beatIntervals.get(i) > maxVal) {
-          maxVal = beatIntervals.get(i);
-        }
-      }
-      return maxVal;
+public void drawIntervalWave(int[] _sampleArray) {
+  int counter = 0;
+  pushStyle();
+  noStroke();
+  fill(0, 100);
+  for (int i=0; i<_sampleArray.length; i++) { //look through the specified set of interval measurements
+    float yPos = _sampleArray[i];
+    yPos = normalizeIntervalVal(_sampleArray, yPos, height); //get and normalize y-value
+    float xPos = map(counter, 0, _sampleArray.length, 30, width-30);
+    ellipse(xPos, yPos, 60, 60);
+    counter++;
+  }
+  popStyle();
+
+}
+
+public float normalizeIntervalVal(int[] _sampleArray, float _arrayVal, int _screenSize) {
+  float _val = map(_arrayVal, 0, maxInterval(_sampleArray), 30, _screenSize-30);
+  return _val;
+}
+
+public float maxInterval(int[] _sampleArray) { //get the largest value in the sample arrray
+  float maxVal = 0;
+  for (int i=0; i<_sampleArray.length; i++) {
+    if (_sampleArray[i] > maxVal) {
+      maxVal = _sampleArray[i];
     }
+  }
+  return maxVal;
+}
 
 public void serialEvent(Serial port){   
    String inData = port.readStringUntil('\n');  
