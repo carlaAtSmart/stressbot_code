4fe943b0b6dd3460bb0b47d46d8be64ae1837314
Steve Faletti <stevefaletti@gmail.com>
2013-06-13 13:14:14 -0400

    IBI Curve tweening added

--- pulSensor_STRESSBOT_20130515/build-tmp/source/pulSensor_STRESSBOT_20130515.java
+++ pulSensor_STRESSBOT_20130515/build-tmp/source/pulSensor_STRESSBOT_20130515.java
@@ -17,20 +17,20 @@ import java.io.IOException;
 public class pulSensor_STRESSBOT_20130515 extends PApplet {
 
 /*     PulseSensor Amped HRV Poncaire Plot
+ 
+ This is an HRV visualizer code for Pulse Sensor.
+ Use this with PulseSensorAmped_Arduino_1.1 Arduino code and the Pulse Sensor Amped hardware.
+ This code will draw a Poncaire Plot of the IBI (InterBeat Interval) passed from Arduino.
+ The Poncaire method of visualizing HRV trends is to plot the current IBI against the last IBI. 
+ key press commands included in this version:
+ press 'S' or 's' to take a picture of the data window. (.jpg image)
+ press 'c' to clear the graph 
+ Created by Joel Murphy, early 2013
+ This code released into the public domain without promises or caveats.
+ */
 
-This is an HRV visualizer code for Pulse Sensor.
-Use this with PulseSensorAmped_Arduino_1.1 Arduino code and the Pulse Sensor Amped hardware.
-This code will draw a Poncaire Plot of the IBI (InterBeat Interval) passed from Arduino.
-The Poncaire method of visualizing HRV trends is to plot the current IBI against the last IBI. 
-key press commands included in this version:
-press 'S' or 's' to take a picture of the data window. (.jpg image)
-press 'c' to clear the graph 
-Created by Joel Murphy, early 2013
-This code released into the public domain without promises or caveats.
-*/
-
-
-Serial port;
+ 
+ Serial port;
 
 int test;                 // general debugger
 int pulseRate = 0;        // used to hold pulse rate value from arduino (updated in serialEvent)
@@ -40,48 +40,53 @@ int ppgY;                 // used to print the pulse waveform
 int maxppgY = 0;
 
 ArrayList<Integer> beatIntervals; //store each beat interval in an Array List so we can compare multiple values over time
+int beatsCount = 22; //number of beats to sample from the ArrayList
 
 // initializing flags here
 boolean pulse = false;    // made true in serialEvent when processing gets new IBI value from arduino
+boolean personIsNear = true; // set true in serialEvent when prox sensor is activated
+boolean fingerIsInserted = true; //set true in serialEvent when photocell is activated
 
 
 public void setup() {
   background(255);
-size(1024, 600, P3D); // Stage size
-// screenMask = loadImage("stressbot-screen-mask.png");
+  size(1024, 600); // Stage size
+  // screenMask = loadImage("stressbot-screen-mask.png");
 
-beatIntervals = new ArrayList(); //create empty array list
+  beatIntervals = new ArrayList(); //create empty array list
 
-// FIND AND ESTABLISH CONTACT WITH THE SERIAL PORT
-println(Serial.list());       // print a list of available serial ports
-port = new Serial(this, Serial.list()[0], 115200); // choose the right baud rate
-port.bufferUntil('\n');          // arduino will end each ascii number string with a carriage return 
-port.clear();                    // flush the Serial buffer
+    // FIND AND ESTABLISH CONTACT WITH THE SERIAL PORT
+  println(Serial.list());       // print a list of available serial ports
+  port = new Serial(this, Serial.list()[0], 115200); // choose the right baud rate
+  port.bufferUntil('\n');          // arduino will end each ascii number string with a carriage return 
+  port.clear();                    // flush the Serial buffer
 }  // END OF SETUP
 
 //----------------------------------------------------------------
 public void draw() {
   background(255);
-  int beatsCount = 20;
-  //TODO: handle face and intro
-if (beatIntervals.size() > beatsCount) { //take beatsCount beats to calibrate
-  int[] intervalSamples = getSampleArray(beatsCount);
-  //TODO: draw sine wave
-  drawIntervalWaveAsBlobs(intervalSamples); //draw teh blob version of the beat intervals
-  drawIntervalWaveAsCurve(intervalSamples); //draw the curve version of the beat intervals
-}  
-}
+  if (personIsNear) {
+    //TODO: draw wakwup animation
+    if (fingerIsInserted) {
+      //TODO: draw intro animation
+      sineCurveStart = drawSineCurve(sineCurveStart);
+      if (beatIntervals.size() > beatsCount) { //take beatsCount beats to calibrate
+        int[] intervalSamples = getSampleArray(beatsCount);
+        ibiCurveStart = drawIntervalWaveAsCurve(intervalSamples, ibiCurveStart); //draw the curve version of the beat intervals
+        }
+      }
+    }
+  }
 
 public int[] getSampleArray(int _sampleArraySize) {
   int[] _sampleIntervals = new int[_sampleArraySize];
   int _counter = 0;
-  for(int i=beatIntervals.size()-_sampleArraySize; i<beatIntervals.size(); i++) {
+  for (int i=beatIntervals.size()-_sampleArraySize; i<beatIntervals.size(); i++) {
     _sampleIntervals[_counter] = beatIntervals.get(i);
     _counter++;
   }
   return _sampleIntervals;
 }
-
 
 public void drawHeartRate() {
   pushStyle();
@@ -92,6 +97,8 @@ public void drawHeartRate() {
   stroke(0);
   rect(width-100, height-100, 70, 70);
   popStyle();
+  noStroke();
+  fill(200);
   ellipse(width-100, height-100, map(ppgY, 0, maxppgY, 10, 50), map(ppgY, 0, maxppgY, 10, 50));
   popStyle();
   // int calcBeatsPerMinute
@@ -102,11 +109,9 @@ public void drawIntervalWaveAsBlobs(int[] _sampleArray) {
   pushStyle();
   noStroke();
   fill(120);
-  // fill(0, map(ppgY, 0, maxppgY, 200, 10));
   for (int i=0; i<_sampleArray.length; i++) { //look through the specified set of interval measurements
     float yPos = map(_sampleArray[i], 0, maxInterval(), height-60, 60);
     float xPos = map(counter, 0, _sampleArray.length, 30, width-30);
-    // ellipse(xPos, yPos, map(ppgY, 0, maxppgY, 10, 50), map(ppgY, 0, maxppgY, 10, 50));
     ellipse(xPos, yPos, 30, 30);
     if (i == _sampleArray.length-1) {
       ellipse(xPos, yPos, map(ppgY, 0, maxppgY, 10, 50), map(ppgY, 0, maxppgY, 10, 50));
@@ -116,20 +121,28 @@ public void drawIntervalWaveAsBlobs(int[] _sampleArray) {
   popStyle();
 }
 
-public void drawIntervalWaveAsCurve(int[] _sampleArray) {
-  int counter = 0;
-  pushStyle();
-  noFill();
-  stroke(0);
-  beginShape();
-  for (int i=0; i<_sampleArray.length; i++) { //look through the specified set of interval measurements
-    float yPos = map(_sampleArray[i], 0, maxInterval(), height-60, 60);
-    float xPos = map(counter, 0, _sampleArray.length, 30, width-30);
-    curveVertex(xPos, yPos);
-    counter++;
-  }
-  endShape();
-  popStyle();
+float ibiCurveStart = 0;
+
+public float drawIntervalWaveAsCurve(int[] _sampleArray, float xStart) {
+  float interval = width/(_sampleArray.length-2); //get the x-interval for each data point but strip the first and last
+  float xPos = xStart-interval; //set the first point off-screen as it is a control point and won't be drawn
+  pushMatrix();
+    translate(0, height/2); //move vertical origin to center of screen. This will likely change to accomodate the frame overlay
+    pushStyle();
+      noFill();
+      strokeWeight(20);
+      smooth();
+      stroke(0);
+      beginShape();
+      for (int i=0; i<_sampleArray.length; i++) {  //step through the set of interval vals
+        float yPos = map(_sampleArray[i], 0, maxInterval(), -150, 150); 
+        curveVertex(xPos, yPos);
+        xPos+=interval;
+      }
+      endShape();
+    popStyle();
+  popMatrix();
+  return xStart-1;
 }
 
 public float maxInterval() { //get the largest value in the sample arrray
@@ -141,40 +154,78 @@ public float maxInterval() { //get the largest value in the sample arrray
   }
   return maxVal;
 }
+// Variables to control background sine wave
+float sineCurveStart = 0;
 
-public void serialEvent(Serial port){   
-   String inData = port.readStringUntil('\n');  
-   
-   if (inData.charAt(0) == 'Q'){          // leading 'Q' means time between beats in milliseconds
-     inData = inData.substring(1);        // cut off the leading 'Q'
-     inData = trim(inData);               // trim the \n off the end
-     IBI = PApplet.parseInt(inData); 
-     addBeat(IBI);                  // convert ascii string to integer IBI 
-     println("IBI: " + IBI);                
-     return;     
-   }
-   
-   if (inData.charAt(0) == 'S'){          // leading 'S' means sensor data
-     inData = inData.substring(1);        // cut off the leading 'S'
-     inData = trim(inData);               // trim the \n off the end
-     ppgY = PApplet.parseInt(inData); 
-     // println("PPG: " + ppgY);
-     if (ppgY > maxppgY) {
-      maxppgY = ppgY;
-     } 
-   return;     
-   }   
-   
-    if (inData.charAt(0) == 'F'){          // leading 'S' means sensor data
-     inData = inData.substring(1);        // cut off the leading 'F'
-     inData = trim(inData);               // trim the \n off the end
-   return;     
-   }  
+public float drawSineCurve(float xStart){
+	float _waveLength = width/5;
+	pushStyle();
+		smooth();
+		noFill();
+		ellipseMode(CENTER);
+		pushMatrix();
+			translate(0, height/2); //move the coordinate system down to the middle of the screen
+			strokeWeight(1);
+			stroke(180);
+			strokeWeight(25);
+			stroke(200);
+			beginShape(); //start drawing the curve
+				float yPos = 150; //height of curve
+				float controlLength = _waveLength/2;
+				vertex(xStart, yPos);
+				for(float i=xStart; i<width+_waveLength; i+=_waveLength){
+					//calculate first control point
+					float cp1X = i+controlLength;
+					//calculate next point on curve
+					float nextPtX = i+_waveLength;
+					//calculate second control point
+					float cp2X = nextPtX-controlLength;
+					bezierVertex(cp1X, yPos, cp2X, yPos*-1, nextPtX, yPos*-1);
+					yPos*=-1;					
+				}
+			endShape();
+		popMatrix();
+	popStyle();
+	return xStart-1;
+}
+
+public void serialEvent(Serial port) {   
+  String inData = port.readStringUntil('\n');  
+
+  if (inData.charAt(0) == 'Q') {          // leading 'Q' means time between beats in milliseconds
+    inData = inData.substring(1);        // cut off the leading 'Q'
+    inData = trim(inData);               // trim the \n off the end
+    IBI = PApplet.parseInt(inData);                  // convert ascii string to integer IBI 
+    beatIntervals.add(IBI);              // add this beat to the ArrayList
+    // println("IBI: " + IBI);                
+    return;
+  }
+
+  if (inData.charAt(0) == 'S') {          // leading 'S' means sensor data
+    inData = inData.substring(1);        // cut off the leading 'S'
+    inData = trim(inData);               // trim the \n off the end
+    ppgY = PApplet.parseInt(inData);                 // convert to integer
+    if (ppgY > maxppgY) maxppgY = ppgY;   // reset the max interval value if needed
+    return;
+  }   
+
+  if (inData.charAt(0) == 'F') {          // leading 'F' means finger data
+    inData = inData.substring(1);        // cut off the leading 'F'
+    inData = trim(inData);               // trim the \n off the end
+    if (PApplet.parseInt(inData) == 1) 
+    fingerIsInserted = true;             //set finger flag
+    return;
+  }
+
+  if (inData.charAt(0) == 'P') {        // leading 'P' means prox sensor data
+    inData = inData.substring(1);       // cut off leading 'P'
+    inData = trim(inData);              // trim the \n off the end
+    if (PApplet.parseInt(inData) == 1) 
+    personIsNear = true;                //set proximity flag
+    return;
+  }
 }// END OF SERIAL EVENT
 
-public void addBeat(int _beatInterval) {
-  beatIntervals.add(_beatInterval);
-}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "pulSensor_STRESSBOT_20130515" };
     if (passedArgs != null) {
--- pulSensor_STRESSBOT_20130515/pulSensor_STRESSBOT_20130515.pde
+++ pulSensor_STRESSBOT_20130515/pulSensor_STRESSBOT_20130515.pde
@@ -1,18 +1,18 @@
 /*     PulseSensor Amped HRV Poncaire Plot
+ 
+ This is an HRV visualizer code for Pulse Sensor.
+ Use this with PulseSensorAmped_Arduino_1.1 Arduino code and the Pulse Sensor Amped hardware.
+ This code will draw a Poncaire Plot of the IBI (InterBeat Interval) passed from Arduino.
+ The Poncaire method of visualizing HRV trends is to plot the current IBI against the last IBI. 
+ key press commands included in this version:
+ press 'S' or 's' to take a picture of the data window. (.jpg image)
+ press 'c' to clear the graph 
+ Created by Joel Murphy, early 2013
+ This code released into the public domain without promises or caveats.
+ */
 
-This is an HRV visualizer code for Pulse Sensor.
-Use this with PulseSensorAmped_Arduino_1.1 Arduino code and the Pulse Sensor Amped hardware.
-This code will draw a Poncaire Plot of the IBI (InterBeat Interval) passed from Arduino.
-The Poncaire method of visualizing HRV trends is to plot the current IBI against the last IBI. 
-key press commands included in this version:
-press 'S' or 's' to take a picture of the data window. (.jpg image)
-press 'c' to clear the graph 
-Created by Joel Murphy, early 2013
-This code released into the public domain without promises or caveats.
-*/
-
-import processing.serial.*;
-Serial port;
+ import processing.serial.*;
+ Serial port;
 
 int test;                 // general debugger
 int pulseRate = 0;        // used to hold pulse rate value from arduino (updated in serialEvent)
@@ -22,48 +22,53 @@ int ppgY;                 // used to print the pulse waveform
 int maxppgY = 0;
 
 ArrayList<Integer> beatIntervals; //store each beat interval in an Array List so we can compare multiple values over time
+int beatsCount = 22; //number of beats to sample from the ArrayList
 
 // initializing flags here
 boolean pulse = false;    // made true in serialEvent when processing gets new IBI value from arduino
+boolean personIsNear = true; // set true in serialEvent when prox sensor is activated
+boolean fingerIsInserted = true; //set true in serialEvent when photocell is activated
 
 
 void setup() {
   background(255);
-size(1024, 600, P3D); // Stage size
-// screenMask = loadImage("stressbot-screen-mask.png");
+  size(1024, 600); // Stage size
+  // screenMask = loadImage("stressbot-screen-mask.png");
 
-beatIntervals = new ArrayList(); //create empty array list
+  beatIntervals = new ArrayList(); //create empty array list
 
-// FIND AND ESTABLISH CONTACT WITH THE SERIAL PORT
-println(Serial.list());       // print a list of available serial ports
-port = new Serial(this, Serial.list()[0], 115200); // choose the right baud rate
-port.bufferUntil('\n');          // arduino will end each ascii number string with a carriage return 
-port.clear();                    // flush the Serial buffer
+    // FIND AND ESTABLISH CONTACT WITH THE SERIAL PORT
+  println(Serial.list());       // print a list of available serial ports
+  port = new Serial(this, Serial.list()[0], 115200); // choose the right baud rate
+  port.bufferUntil('\n');          // arduino will end each ascii number string with a carriage return 
+  port.clear();                    // flush the Serial buffer
 }  // END OF SETUP
 
 //----------------------------------------------------------------
 void draw() {
   background(255);
-  int beatsCount = 20;
-  //TODO: handle face and intro
-if (beatIntervals.size() > beatsCount) { //take beatsCount beats to calibrate
-  int[] intervalSamples = getSampleArray(beatsCount);
-  //TODO: draw sine wave
-  drawIntervalWaveAsBlobs(intervalSamples); //draw teh blob version of the beat intervals
-  drawIntervalWaveAsCurve(intervalSamples); //draw the curve version of the beat intervals
-}  
-}
+  if (personIsNear) {
+    //TODO: draw wakwup animation
+    if (fingerIsInserted) {
+      //TODO: draw intro animation
+      sineCurveStart = drawSineCurve(sineCurveStart);
+      if (beatIntervals.size() > beatsCount) { //take beatsCount beats to calibrate
+        int[] intervalSamples = getSampleArray(beatsCount);
+        ibiCurveStart = drawIntervalWaveAsCurve(intervalSamples, ibiCurveStart); //draw the curve version of the beat intervals
+        }
+      }
+    }
+  }
 
 int[] getSampleArray(int _sampleArraySize) {
   int[] _sampleIntervals = new int[_sampleArraySize];
   int _counter = 0;
-  for(int i=beatIntervals.size()-_sampleArraySize; i<beatIntervals.size(); i++) {
+  for (int i=beatIntervals.size()-_sampleArraySize; i<beatIntervals.size(); i++) {
     _sampleIntervals[_counter] = beatIntervals.get(i);
     _counter++;
   }
   return _sampleIntervals;
 }
-
 
 void drawHeartRate() {
   pushStyle();
@@ -74,6 +79,8 @@ void drawHeartRate() {
   stroke(0);
   rect(width-100, height-100, 70, 70);
   popStyle();
+  noStroke();
+  fill(200);
   ellipse(width-100, height-100, map(ppgY, 0, maxppgY, 10, 50), map(ppgY, 0, maxppgY, 10, 50));
   popStyle();
   // int calcBeatsPerMinute
@@ -84,11 +91,9 @@ void drawIntervalWaveAsBlobs(int[] _sampleArray) {
   pushStyle();
   noStroke();
   fill(120);
-  // fill(0, map(ppgY, 0, maxppgY, 200, 10));
   for (int i=0; i<_sampleArray.length; i++) { //look through the specified set of interval measurements
     float yPos = map(_sampleArray[i], 0, maxInterval(), height-60, 60);
     float xPos = map(counter, 0, _sampleArray.length, 30, width-30);
-    // ellipse(xPos, yPos, map(ppgY, 0, maxppgY, 10, 50), map(ppgY, 0, maxppgY, 10, 50));
     ellipse(xPos, yPos, 30, 30);
     if (i == _sampleArray.length-1) {
       ellipse(xPos, yPos, map(ppgY, 0, maxppgY, 10, 50), map(ppgY, 0, maxppgY, 10, 50));
@@ -98,20 +103,28 @@ void drawIntervalWaveAsBlobs(int[] _sampleArray) {
   popStyle();
 }
 
-void drawIntervalWaveAsCurve(int[] _sampleArray) {
-  int counter = 0;
-  pushStyle();
-  noFill();
-  stroke(0);
-  beginShape();
-  for (int i=0; i<_sampleArray.length; i++) { //look through the specified set of interval measurements
-    float yPos = map(_sampleArray[i], 0, maxInterval(), height-60, 60);
-    float xPos = map(counter, 0, _sampleArray.length, 30, width-30);
-    curveVertex(xPos, yPos);
-    counter++;
-  }
-  endShape();
-  popStyle();
+float ibiCurveStart = 0;
+
+float drawIntervalWaveAsCurve(int[] _sampleArray, float xStart) {
+  float interval = width/(_sampleArray.length-2); //get the x-interval for each data point but strip the first and last
+  float xPos = xStart-interval; //set the first point off-screen as it is a control point and won't be drawn
+  pushMatrix();
+    translate(0, height/2); //move vertical origin to center of screen. This will likely change to accomodate the frame overlay
+    pushStyle();
+      noFill();
+      strokeWeight(20);
+      smooth();
+      stroke(0);
+      beginShape();
+      for (int i=0; i<_sampleArray.length; i++) {  //step through the set of interval vals
+        float yPos = map(_sampleArray[i], 0, maxInterval(), -150, 150); 
+        curveVertex(xPos, yPos);
+        xPos+=interval;
+      }
+      endShape();
+    popStyle();
+  popMatrix();
+  return xStart-1;
 }
 
 float maxInterval() { //get the largest value in the sample arrray
